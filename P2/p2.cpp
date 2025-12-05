fcb3b5bef7b8e8b797fbe4cb93e478f94cac2a7d
mr-boone <marcoeboone@gmail.com>
2025-12-05 16:12:13 -0500

    recent

--- P2/p2.cpp
+++ P2/p2.cpp
@@ -1,18 +1,67 @@
 #include <iostream>
 #include <vector>
+using namespace std;
+
+void numToVec(long long num, vector<int> &vec)
+{
+    vec.clear();       
+    while (num > 0) {
+        vec.push_back(num % 10);
+        num /= 10;
+    }
+}
+
+bool valid(vector<int> vec)
+{
+    int validCount = 0; 
+    for(int i = 1; i <= vec.size()/2; i++)
+    {
+        if(vec.size()%i != 0) 
+        {
+            validCount++; 
+            continue; 
+        }  
+        int base = i; 
+        for(int k = 0; k < vec.size()/i; k++)
+        {
+         if(vec[k] != vec[(k+base)])
+         {
+            validCount++ ; 
+            break; 
+         } 
+        }
+    }
+    return validCount == vec.size()/2;
+}
 
 int main() {
-    std::vector<int> nums;
-    int lowerBound;
-    int upperBound; 
-    char dummy; 
+    long long lowerBound;
+    long long upperBound;
+    char dash;
+    char comma;
 
-    // Read integers from stdin until EOF
-    while (std::cin >> lowerBound) {
-        std::cin >> dummy;
-        std::cin >> upperBound; 
-        std::cout << lowerBound << "-" << upperBound << std::endl; 
+    vector<int> lb_vec;
+    vector<int> ub_vec;
+    long long invalidSum = 0; 
+
+    while (cin >> lowerBound >> dash >> upperBound) {
+
+     
+        while(lowerBound <= upperBound)
+        {
+            numToVec(lowerBound, lb_vec);
+            numToVec(upperBound, ub_vec);
+            if(!valid(lb_vec)) 
+            {
+                invalidSum +=lowerBound;
+                cout << lowerBound << "\n";  
+            }
+            lowerBound++; 
+        }
+
+        cin >> comma;
+
     }
 
-   
+    cout << invalidSum << "\n"; 
 }
